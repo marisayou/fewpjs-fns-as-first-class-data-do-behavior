04c26ccbda9754ec2236f2052a1e1066f8818e12
Marisa You <marisayou2017@u.northwestern.edu>
2020-11-16 14:09:03 -0800

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  let hour = parseInt(time.split(0, 2))
+  if (hour < 12) {
+    return "Good Morning"
+  }
+  else if (hour < 17) {
+    return "Good Afternoon"
+  }
+  else {
+    return "Good Evening"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(msg) {
+  let node = document.querySelector("#greeting");
+  node.innerText = msg;
+}
